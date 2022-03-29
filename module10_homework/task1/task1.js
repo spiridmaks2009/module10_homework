0b001b30a61775c7dc86a8b97cd376cbe152d352
Спиридонов Максим <m.spiridonov@zebrains.team>
2022-03-29 15:41:57 +0300

    Task 1

---
+++ module10_homework/task1/task1.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('.btn');
+const blackBtn =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"\n' +
+    '     class="bi bi-arrow-down-left-circle-fill" viewBox="0 0 16 16">\n' +
+    '     <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-5.904-2.803a.5.5 0 1 1 .707.707L6.707 10h2.768a.5.5 0 0 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.525a.5.5 0 0 1 1 0v2.768l4.096-4.096z"/>\n' +
+    '</svg>';
+const whiteBtn =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"\n' +
+    '     class="bi bi-arrow-down-left-circle" viewBox="0 0 16 16">\n' +
+    '     <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768l4.096-4.097z"/>\n' +
+    '</svg>'
+let isWhiteBtn = false;
+
+window.onload = function () {
+    button.innerHTML = whiteBtn;
+    isWhiteBtn = true;
+}
+
+button.addEventListener('click', async () => {
+
+    if (isWhiteBtn) {
+        button.innerHTML = blackBtn;
+        isWhiteBtn = false;
+    } else {
+        button.innerHTML = whiteBtn;
+        isWhiteBtn = true;
+    }
+    console.log('end');
+})
